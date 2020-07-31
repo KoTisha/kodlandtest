5492883b95c638ec91af84752910fd1d0cca1f70
Konstantin Tishkov <konstantin-tishkov@hotmail.com>
2020-07-31 12:54:30 +0500

    Added model & form

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=350)
+    text = models.TextField()
+    date = models.DateTimeField(editable=False)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date = timezone.now()
+        return super(Post, self).save(*args, **kwargs)
