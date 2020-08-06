0f4a385ba46b50b587e3b9c76059e920ac798d2e
Konstantin Tishkov <konstantin-tishkov@hotmail.com>
2020-08-06 17:37:55 +0500

    Heroku update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kodlandtest.wsgi
--- kodlandtest/settings.py
+++ kodlandtest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 from kodlandtest.security import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -141,3 +141,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
