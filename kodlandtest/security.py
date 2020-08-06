0cb423bd307a12b94ac5c6edf615d93cee491ee6
Konstantin Tishkov <konstantin-tishkov@hotmail.com>
2020-08-06 17:00:19 +0500

    Небольшие изменения для работы с Heroku

---
+++ kodlandtest/security.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY = 'rb%zgstuedw!(ena*7($tgul#inl3g9_#*%4)_o8h3nj(9nb!z'
+
+DATABASE_SECRET = {
+    'NAME': 'koda_db',
+    'USER': 'koda',
+    'PASSWORD': 'kodatest',
+    'HOST': '185.253.219.55',
+    'PORT': '5429',
+}
